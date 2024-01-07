d24372590e9203754442c6d0974113f7b8eee6c5
10Sadique <jafarsjahan@gmail.com>
2024-01-07 17:48:25 +0600

    database connection setup

---
+++ src/db/schemas/user.ts
@@ -0,0 +1,10 @@
+import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
+
+export const users = sqliteTable("user", {
+  id: text("id").notNull().primaryKey(),
+  name: text("name"),
+  email: text("email").notNull(),
+  emailVerified: integer("emailVerified", { mode: "timestamp_ms" }),
+  image: text("image"),
+  password: text("password"),
+});
